256c440da89be6d37b9964e605b16d1dd933dfa9
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-18 06:49:06 +0300

    Add files via upload

---
+++ splash.h
@@ -0,0 +1,10 @@
+
+#define splash1_width 128
+#define splash1_height 32
+
+const uint8_t PROGMEM splash1_data[] = {};
+
+#define splash2_width 128
+#define splash2_height 32
+
+const uint8_t PROGMEM splash2_data[] = {};
